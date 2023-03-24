b7b2847258c354ff1dcf982a57335bf392300c66
vivek286 <solankivivek01@gmail.com>
2023-03-25 02:53:06 +0530

    fixed firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyA6jXw-vy_EmY0QXaL2WTo74XQjMe2K5D0",
+  authDomain: "convin-24841.firebaseapp.com",
+  projectId: "convin-24841",
+  storageBucket: "convin-24841.appspot.com",
+  messagingSenderId: "730751857999",
+  appId: "1:730751857999:web:25df65118fc8111d16413a",
+  measurementId: "G-58XMJ4DWV2"
 };
 
 // Initialize Firebase
